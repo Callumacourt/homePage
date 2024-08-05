840997811c6a9f6c6755985f1c6502c0335b052c
Callum A'court <callumacourtt@gmail.com>
2024-08-05 20:29:44 +0100

    fixed image import one

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import portrait from './assets/images/portrait.jpg';
+
+const portraitContainer = document.querySelector('.imageContainer');
+
+const myPortrait = new Image();
+myPortrait.src = portrait;
+portraitContainer.appendChild(myPortrait);
